48b5f78ad0645a3d593c6b868cd2fd0c4f74275e
Diego Sosa <diego.stt@live.com>
2016-08-13 18:04:19 -0600

    Angular

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
-var path = require('path');
+var path         = require('path');
 
 var configDB = require('./config/database.js');
 
