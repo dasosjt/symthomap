6880a0319ba17e224c5f063f9e213dc7ac0e3831
Diego Sosa <diego.stt@live.com>
2016-08-14 20:06:24 -0600

    Angular fresh start

--- server.js
+++ server.js
@@ -34,9 +34,6 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(methodOverride('X-HTTP-Method-Override'));
-
 // set the static files location /public/img will be /img for users
 app.use(express.static(__dirname + '/public'));
 
