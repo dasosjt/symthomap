406d0f77d7da2bc6e1aaa45c841bd54192732036
Luisa2125 <luiyi2125@gmail.com>
2016-08-18 15:11:51 -0600

    tAngular

--- public/js/services/NerdService.js
+++ public/js/services/NerdService.js
@@ -2,18 +2,22 @@
 angular.module('NerdService', []).factory('Nerd', ['$http', function($http) {
 
     return {
-        
+
+        //llamada al metodo GET  de PHP por medio de angular
         get : function() {
             return $http.get('/api/nerds');
         },
 
 
-
+       
+        //llamada al metodo POST de PHP por medio de angular
         create : function(nerdData) {
             return $http.post('/api/nerds', nerdData);
         },
 
-
+        
+        
+        //llamada al metodo DELETE  de PHP por medio de angular
         delete : function(id) {
             return $http.delete('/api/nerds/' + id);
         }
