f7620ca049bdaace946c9e88b0b25089bbab5adb
Diego Sosa <diego.stt@live.com>
2016-08-23 16:36:44 -0600

    login

---
+++ public/js/controllers/LoginCtrl.js
@@ -0,0 +1,6 @@
+// public/js/controllers/NerdCtrl.js
+angular.module('LoginCtrl', []).controller('loginController', function($scope) {
+
+    $scope.tagline = 'Nothing beats a pocket protector!';
+
+});
