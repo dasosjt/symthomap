b35094a975f75d53f4cd3b8a4b8a176df6197e4c
Diego Sosa <diego.stt@live.com>
2016-08-23 17:13:07 -0600

    login

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,2 @@
 // public/js/app.js
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'PhoneCtrl']);
+angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService']);
