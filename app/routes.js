d2d626ced57ace42f6cb3be8f497fb6b9d14ad40
Diego Sosa <diego.stt@live.com>
2016-08-30 11:52:49 -0600

    pto 3306

--- app/routes.js
+++ app/routes.js
@@ -73,7 +73,7 @@ module.exports = function(app, passport) {
      var config = {
              user: 'u234902799_jhon',
              password: 'jhonjacobs',
-             server: 'http://sql12.hostinger.es/',
+             server: 'http://sql12.hostinger.es/:3306',
              database: 'u234902799_stmp'
            };
      sql.connect(config, function (err) {
