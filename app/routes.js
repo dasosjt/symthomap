3177770b87709e20e095acfd9c36b3459a894e79
Diego Sosa <diego.stt@live.com>
2016-08-30 12:04:20 -0600

    port

--- app/routes.js
+++ app/routes.js
@@ -73,7 +73,7 @@ module.exports = function(app, passport) {
      var config = {
              user: 'u234902799_jhon',
              password: 'jhonjacobs',
-             server: 'http://sql12.hostinger.es',
+             server: 'mysql.hostinger.es',
              database: 'u234902799_stmp',
              port: 3306,
            };
