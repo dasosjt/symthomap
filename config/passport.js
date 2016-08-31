4f2b894d497c581ef6e803c0295971f65a84b4c1
Diego Sosa <diego.stt@live.com>
2016-08-31 14:05:09 -0600

    Passport auth

--- config/passport.js
+++ config/passport.js
@@ -68,6 +68,7 @@ module.exports = function(passport) {
             if (err) throw err;
 
             console.log('Rows: ', rows);
+            console.log('Rows Size : ', rows.size());
 
             if(rows){
               console.log('User exists');
