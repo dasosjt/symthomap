6c0a1e64e83c665f942b6ff88f068afe1216d398
Link-djst <diego.stt@live.com>
2016-09-28 18:14:17 -0600

    connection Angular v 1.09

--- app/routes.js
+++ app/routes.js
@@ -78,7 +78,7 @@ module.exports = function(app, passport) {
       });
       var temp;
       connection.query("SELECT * FROM heroku_03080da74f6c5f8.patient ", function(err, rows) {
-        temp = rows;
+        temp = rows[0].name;
         console.log("Name ", rows[0].name);
         console.log("ROWS FKFKFKFKFKF ",rows);
         if (err) {
