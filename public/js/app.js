9d7d88a7969230334bd562ba35b8619706fdee17
Link-djst <diego.stt@live.com>
2016-11-12 19:59:49 -0600

    delete

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,2 @@
 // public/js/app.js
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'SignUpCtrl','DashboardCtrl', 'uiGmapgoogle-maps']);
+angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'SignUpCtrl','DashboardCtrl']);
