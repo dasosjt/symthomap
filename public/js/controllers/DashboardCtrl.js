d94320a195c5eeada1f3c094ace94ca7735ae9eb
Link-djst <diego.stt@live.com>
2016-11-12 19:02:16 -0600

    added ngmap

--- public/js/controllers/DashboardCtrl.js
+++ public/js/controllers/DashboardCtrl.js
@@ -1,7 +1,7 @@
 /**
  * Created by Pablo on 10/4/2016.
  */
-angular.module('DashboardCtrl', []).controller('DashboardController', function($scope, $http) {
+angular.module('DashboardCtrl', []).controller('DashboardController', function($scope, $http, NgMap) {
     $scope.tagline = {};
     $http.get('/patient')
         .success(function(data) {
@@ -11,6 +11,7 @@ angular.module('DashboardCtrl', []).controller('DashboardController', function($
         .error(function(data) {
             console.log('Error: ' + data);
         });
+
     $http.get('/dashboard/user')
         .success(function(data) {
             console.log(data.user);
@@ -30,37 +31,10 @@ angular.module('DashboardCtrl', []).controller('DashboardController', function($
         .error(function(data) {
             console.log('Error: ' + data);
         });
-    $scope.map = {center: {latitude: 14.6284, longitude: -90.5227 }, zoom: 4, bounds: {}};
-    $scope.polygons = [
-        {
-            id: 1,
 
-            path: [
-                {
-                    latitude: 14.0138,
-                    longitude: -91.2867
-                },
-                {
-                    latitude: 15.9621,
-                    longitude: -91.2867
-                },
-                {
-                    latitude: 15.5144,
-                    longitude: -90.237297
-                }
-            ],
-            stroke: {
-                color: '#6060FB',
-                weight: 3
-            },
-            editable: true,
-            draggable: true,
-            geodesic: false,
-            visible: true,
-            fill: {
-                color: '#ff0000',
-                opacity: 0.8
-            }
-        }
-    ];
+    NgMap.getMap().then(function(map) {
+        console.log(map.getCenter());
+        console.log('markers', map.markers);
+        console.log('shapes', map.shapes);
+    });
 });
