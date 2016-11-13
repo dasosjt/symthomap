b1b01aa298871e9abc23e21d449baf21fce68a2a
Link-djst <diego.stt@live.com>
2016-11-13 17:01:21 -0600

    NgMap

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,2 @@
 // public/js/app.js
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'SignUpCtrl','DashboardCtrl', 'NgMap']);
+angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'SignUpCtrl','DashboardCtrl']);
