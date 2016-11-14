d1ea8baef522217242083c7ebb673bfb69ea60ac
Link-djst <diego.stt@live.com>
2016-11-14 15:53:58 -0600

    map

--- public/js/controllers/DashboardCtrl.js
+++ public/js/controllers/DashboardCtrl.js
@@ -1,9 +1,13 @@
 /**
  * Created by Pablo on 10/4/2016.
  */
-angular.module('DashboardCtrl', []).controller('DashboardController', function($scope, $http) {
+angular.module('DashboardCtrl', []).controller('DashboardController', function($scope, $http, NgMap) {
     $scope.tagline = {};
-
+    NgMap.getMap().then(function(map) {
+      console.log(map.getCenter());
+      console.log('markers', map.markers);
+      console.log('shapes', map.shapes);
+    });
     $http.get('/patient')
         .success(function(data) {
             $scope.names = data.patients;
